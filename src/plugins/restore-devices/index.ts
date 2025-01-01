64fda3b0a5a373571b4cf19c6e20f083dd102fae
nexpid <60316309+nexpid@users.noreply.github.com>
2025-01-01 15:06:54 +0100

    feat(plugins): add `restore-devices` plugin
    Co-authored-by: PalmDevs <git@palmdevs.me>

---
+++ src/plugins/restore-devices/index.ts
@@ -0,0 +1,21 @@
+import { registerPlugin } from '@revenge-mod/plugins/internals'
+
+registerPlugin(
+    {
+        name: 'Restore Devices Setting',
+        author: 'Palm',
+        description: 'Brings the Devices settings page back',
+        id: 'palmdevs.restore-devices-setting',
+        version: '1.0.0',
+        icon: 'LaptopPhoneIcon',
+        beforeAppRender({ patcher, revenge: { modules } }) {
+            const DevicesSetting = modules.findByFilePath<{ usePredicate(): boolean }, true>(
+                'modules/main_tabs_v2/native/settings/definitions/DevicesSetting.tsx',
+                true,
+            )
+
+            patcher.instead(DevicesSetting!, 'usePredicate', () => true)
+        },
+    },
+    true,
+)
